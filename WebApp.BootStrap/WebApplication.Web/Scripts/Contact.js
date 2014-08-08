ce9d4550d47b18dd0a992932ad494a519d7a7eb9
fran.gori90@gmail.com <pakito13>
2014-08-07 23:48:40 -0500

    Nuevo proyecto web

---
+++ WebApp.BootStrap/WebApplication.Web/Scripts/Contact.js
@@ -0,0 +1,32 @@
+﻿var DummyProfile = [
+    {
+        "ProfileId": 1,
+        "FirstName": "Anna",
+        "LastName": "Pandey",
+        "Email": "anand@anandpandey.com"
+    },
+    {
+        "ProfileId": 2,
+        "FirstName": "John",
+        "LastName": "Cena",
+        "Email": "john@cena.com"
+    },
+    {
+        "ProfileId": 3,
+        "FirstName": "Pako",
+        "LastName": "Rivera",
+        "Email": "rivera@mail.com"
+    }
+]
+
+var ProfilesViewModel = function () {
+    var self = this;
+    var refresh = function () {
+        self.Profiles(DummyProfile);
+    };
+
+    // Public data properties
+    self.Profiles = ko.observableArray([]);
+    refresh();
+};
+ko.applyBindings(new ProfilesViewModel());
